3aff0285c04004419fec72006d1a312f1accd94f
Mayankgupta645 <mayank.gupta1.mm@gmail.com>
2025-07-10 01:20:32 +0530

    updated the database

--- database.js
+++ database.js
@@ -2,7 +2,10 @@ const mongoose= require('mongoose');
 require('dotenv').config();
 const mongodURL=process.env.dblocal;
 // const mongodURL=process.env.db;
-mongoose.connect(mongodURL);
+mongoose.connect(mongodURL,{
+    useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 const db=mongoose.connection;
 db.on('connected',()=>{
     console.log("mongod is connected");
