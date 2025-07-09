9bed3457c635911b032661f39cda3969baab850a
Mayankgupta645 <mayank.gupta1.mm@gmail.com>
2025-07-09 23:30:48 +0530

    fourth version

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const mongoose= require('mongoose');
 require('dotenv').config();
-// const mongodURL='mongodb://localhost:27017/films'
-const mongodURL=process.env.db;
+const mongodURL=process.env.dblocal;
+// const mongodURL=process.env.db;
 mongoose.connect(mongodURL);
 const db=mongoose.connection;
 db.on('connected',()=>{
