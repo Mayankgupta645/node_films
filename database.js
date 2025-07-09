eab94cb87da70d8a73eed8a915c4ef3fc8fbcd01
Mayankgupta645 <mayank.gupta1.mm@gmail.com>
2025-07-10 01:35:36 +0530

    updated the database again for more correction and security

--- database.js
+++ database.js
@@ -2,10 +2,7 @@ const mongoose= require('mongoose');
 require('dotenv').config();
 // const mongodURL=process.env.dblocal;
 const mongodURL=process.env.db;
-mongoose.connect(mongodURL,{
-    useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(mongodURL);
 const db=mongoose.connection;
 db.on('connected',()=>{
     console.log("mongod is connected");
